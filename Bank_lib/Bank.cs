b35f95af03f936a6c6837b42482e1c9f19c89217
Christopher hardamek <christopherhardamek@Christophers-MacBook-Pro.local>
2022-03-28 08:41:59 -0600

    href for accounts

--- Bank_lib/Bank.cs
+++ Bank_lib/Bank.cs
@@ -8,8 +8,11 @@ public static class Bank
     static public List<Account> Accounts{get;set;} = new();
     public static void LoadAccounts()
     {
+        if (File.Exists("accounts.txt")){
+
         var json = File.ReadAllText("accounts.json");
         Accounts = JsonSerializer.Deserialize<List<Account>>(json);
+        }
     }
     public static void SaveAccounts()
     {
--- Bank_lib/Customer.cs
+++ Bank_lib/Customer.cs
@@ -7,18 +7,26 @@ using System.IO;
 namespace Banksystem;
 public class Customer : IValidate
 {
-    public string Name { get; set; }
+    public string Name;
     public string Lastname { get; set; }
     public int Age { get; set; }
     public DateTime Birthdate { get; set; }
     public int Balance = 0;
 
+    public string getName()
+    {
+        return Name;
+    }
+    public void setName(string name)
+    {
+        Name = name;
+    }
     public int getBalance()
     {
         return Balance;
     }
-
-
+    private List<Account> accounts;
+    public IEnumerable<Account> Accounts => accounts;
     public Customer(string name, string LastName, int day, int month, int year)
     {
         if (ValidateLastName(LastName) == true)
@@ -27,7 +35,6 @@ public class Customer : IValidate
         }
         if (ValidateName(name))
         {
-
             Name = name;
         }
         Birthdate = new DateTime(year, month, day);
