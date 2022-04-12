f0f3e73c06d9307f788e785ab9a4910f4bcbb8cd
Christopher hardamek <christopherhardamek@Christophers-MacBook-Pro.local>
2022-04-12 16:54:38 -0600

    Transfer money between accounts

--- Bank_lib/Customer.cs
+++ Bank_lib/Customer.cs
@@ -12,12 +12,14 @@ public class Customer : IValidate
     public int Age { get; set; }
     public DateTime Birthdate { get; set; }
     public decimal CheckingBalance { get; set; }
-    public bool savingaccount;
+    public bool savingaccount { get; set; }
     public decimal SavingBalance { get; set; }
     public string log;
 
 
-    public Customer(string name, string LastName, int day, int month, int year, decimal CheckingBalance, bool Savingaccount)
+    public Customer(string name, string LastName,
+    int day, int month, int year, decimal CheckingBalance,
+    bool Savingaccount, decimal SavingBalance)
     {
         if (ValidateLastName(LastName) == true)
         {
@@ -30,25 +32,27 @@ public class Customer : IValidate
         savingaccount = Savingaccount;
 
         Birthdate = new DateTime(year, month, day);
+        this.savingaccount = Savingaccount;
         this.CheckingBalance = CheckingBalance;
+        this.SavingBalance = SavingBalance;
 
     }
     public Customer() { }
 
 
-    public void MakeAccount(string name, string LastName, int day, int month, int year, decimal Checkingbalance, bool Savingaccount)
+    public void MakeAccount(string name, string LastName,
+                            int day, int month, int year,
+                            decimal Checkingbalance, bool Savingaccount,
+                            decimal savingBalance)
     {
-        Customer customer = new Customer(name, LastName, day, month, year, Checkingbalance, Savingaccount);
+        Customer customer = new Customer(name, LastName, day, month, year, Checkingbalance, Savingaccount, savingBalance);
         var account = new Account(customer);
         logging.logs.Add($"Added account for {name} on {DateTime.Now}");
         logging.Savelog();
         Bank.Accounts.Add(account);
         Bank.SaveAccounts();
     }
-    public bool MakeSavingAccount()
-    {
-        return savingaccount = true;
-    }
+
 
 
     public bool ValidateLastName(string LastName)
@@ -100,9 +104,9 @@ public class Customer : IValidate
         {
             if (customer.Owner.Name == Name)
             {
-                if ((CheckingBalance - depositAmount)  <= -500)
+                if ((CheckingBalance - depositAmount) <= -500)
                 {
-                    CheckingBalance -= depositAmount;
+                    customer.Owner.CheckingBalance -= depositAmount;
                     log = $"Substract money {depositAmount} to {Name} new Balance: {CheckingBalance} on {DateTime.Now}";
                     logging.logs.Add(log);
                     logging.Savelog();
@@ -110,5 +114,52 @@ public class Customer : IValidate
                 break;
             }
         }
+    }
+    public void MakeSavingAccount()
+    {
+        foreach (var customer in Bank.Accounts)
+        {
+            if (customer.Owner.Name == Name)
+            {
+                customer.Owner.savingaccount = true;
+                Bank.SaveAccounts();
+                logging.logs.Add($"Savingaccount created for {Name} on {DateTime.Now}");
+                logging.Savelog();
+                break;
+            }
+
+        }
+        Bank.SaveAccounts();
+    }
+    public void TransferMoneyFromCheckingToSaving(decimal deposit)
+    {
+        foreach (var customer in Bank.Accounts)
+        {
+            if (customer.Owner.Name == Name)
+            {
+                customer.Owner.CheckingBalance -= deposit;
+                customer.Owner.SavingBalance += deposit;
+                break;
+            }
+
+        }
+        Bank.SaveAccounts();
+
+    }
+    public void TransferMoneyFromSavinToChecking(decimal deposit)
+    {
+        foreach (var customer in Bank.Accounts)
+        {
+            if (customer.Owner.Name == Name)
+            {
+                customer.Owner.CheckingBalance = +deposit;
+                customer.Owner.SavingBalance = -deposit;
+                Bank.SaveAccounts();
+                break;
+            }
+
+        }
+        Bank.SaveAccounts();
+
     }
 }
